08df8450785386c9b06c7f2894311f5099fdffc7
Manoa Andrianaivojaona Rason <titibeloha@Manoas-MacBook-Pro.local>
2022-03-20 23:33:36 +0100

    repository: Subreddit repository

---
+++ src/main/java/com/manrason/reddit/repository/SubredditRepository.java
@@ -0,0 +1,9 @@
+package com.manrason.reddit.repository;
+
+import com.manrason.reddit.model.Subreddit;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SubredditRepository extends JpaRepository<Subreddit,Long> {
+}
