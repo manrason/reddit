db548639456f7887b8a36fca30a75e70e20d8e4e
Manoa Andrianaivojaona Rason <titibeloha@tl-wa850re-1.home>
2022-03-21 00:16:36 +0100

    dto: register request class

--- src/main/java/com/manrason/reddit/dto/RegisterRequest.java
+++ src/main/java/com/manrason/reddit/dto/RegisterRequest.java
@@ -1,2 +1,14 @@
-package com.manrason.reddit.dto;public class RegisterRequest {
+package com.manrason.reddit.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+    private String email;
+    private String username;
+    private String password;
 }
