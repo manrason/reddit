0f780d3dba4553252f95dd405ec3f526515aa003
Manoa Andrianaivojaona Rason <titibeloha@Manoas-MacBook-Pro.local>
2022-03-20 23:35:02 +0100

    repository: User repository

---
+++ src/main/java/com/manrason/reddit/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.manrason.reddit.repository;
+
+import com.manrason.reddit.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+}
