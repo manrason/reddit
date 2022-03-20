2a5571e03ef767763c92955b9180808fc35b3cbc
Manoa Andrianaivojaona Rason <titibeloha@Manoas-MacBook-Pro.local>
2022-03-20 23:37:01 +0100

    repository: Comment repository

---
+++ src/main/java/com/manrason/reddit/repository/CommentRepository.java
@@ -0,0 +1,9 @@
+package com.manrason.reddit.repository;
+
+import com.manrason.reddit.model.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment,Long> {
+}
