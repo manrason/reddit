5d95187338984cbe483332ad4e93e07a36fcc84a
Manoa Andrianaivojaona Rason <titibeloha@Manoas-MacBook-Pro.local>
2022-03-20 20:21:49 +0100

    modifying main

--- src/main/java/com/manrason/reddit/RedditApplication.java
+++ src/main/java/com/manrason/reddit/RedditApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RedditApplication {
 
 	public static void main(String[] args) {
+		System.out.println("reddit clone");
 		SpringApplication.run(RedditApplication.class, args);
 	}
 
