1b1dc176e9aa8b7332c0fd78fe3a01c2ab6a217a
Manoa Andrianaivojaona Rason <titibeloha@Manoas-MacBook-Pro.local>
2022-03-20 23:36:12 +0100

    repository: Vote repository

---
+++ src/main/java/com/manrason/reddit/repository/VoteRepository.java
@@ -0,0 +1,9 @@
+package com.manrason.reddit.repository;
+
+import com.manrason.reddit.model.Vote;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VoteRepository extends JpaRepository<Vote,Long> {
+}
